f2c1eaf1ec88800578d5abb09d56498c2569be07
Dmitry Sigaev <www@sigaev.com>
2011-09-03 18:31:50 -0400

    Use git to patch

--- make-config.sh
+++ make-config.sh
@@ -1,9 +1,9 @@
 cat <<END
 sudo yum -y update
-sudo yum -y install git httpd patch
+sudo yum -y install git httpd
 chmod 755 .
 mkdir www
-sudo patch -d/ -p1 <<EOF
+sudo git apply --directory=/ <<EOF
 `git diff master...patch`
 EOF
 sudo /sbin/chkconfig httpd on
