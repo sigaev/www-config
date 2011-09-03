ea624415fb69071d2f60880a905d1d9196b35dab
Dmitry Sigaev <www@sigaev.com>
2011-09-03 18:01:48 -0400

    Use ... not .. since it is safer when branches diverge

--- make-config.sh
+++ make-config.sh
@@ -4,7 +4,7 @@ sudo yum -y install git httpd patch
 chmod 755 .
 mkdir www
 sudo patch -d/ -p1 <<EOF
-`git diff master..patch`
+`git diff master...patch`
 EOF
 sudo /sbin/chkconfig httpd on
 sudo /sbin/service httpd start
