6c34dc2afdc409fd10b9c785c519a3fd82f3cdbf
Dmitry Sigaev <www@sigaev.com>
2012-08-09 12:54:14 -0400

    Fix some bugs

--- cfg-gentoo.sh
+++ cfg-gentoo.sh
@@ -6,24 +6,24 @@ cd /
 wget s3.amazonaws.com/sigaev/linux/$1 || exit 1
 wget -qO- s3.amazonaws.com/sigaev/linux/unsquashfs.txz | tar xJC media || exit 1
 media/lib/ld-2* --library-path media/lib:media/usr/lib \
-	media/usr/bin/unsquashfs -f -d mnt *.sfs || exit 1
+	media/usr/bin/unsquashfs -f -d mnt $1 || exit 1
 
-cat >media/init <<'EOF'
+cat >media/init <<EOF
 #!/bin/bash
 
 old() {
-	echo Starting \[init~ "$@"\] >&2
-	exec sbin/init~ "$@"
+	echo Starting \\[init~ "\$@"\\] >&2
+	exec sbin/init~ "\$@"
 }
 
 if [[ -e proc/uptime ]]; then
-	ut=`tr . \  < proc/uptime | cut -d\  -f1`
-	echo Uptime: $ut >&2
-	if [ 99 -lt $ut ]; then
-		old "$@"
+	ut=\`tr . \\  < proc/uptime | cut -d\\  -f1\`
+	echo Uptime: \$ut >&2
+	if [ 99 -lt \$ut ]; then
+		old "\$@"
 	fi
 else
-	old "$@"
+	old "\$@"
 fi
 
 if [[ ! -e $1 ]]; then
@@ -37,11 +37,11 @@ rm -f mnt/lib/modules
 mv lib/modules mnt/lib/
 mv etc/ssh/ssh_host_* mnt/etc/ssh/
 
-rm -fr mnt/{dev,proc,sys,mnt} `ls | egrep -v 'lost.found|dev|proc|sys|mnt'`
+rm -fr mnt/{dev,proc,sys,mnt} \`ls | egrep -v 'lost.found|dev|proc|sys|mnt'\`
 mnt/lib/ld-2* --library-path mnt/lib mnt/bin/mv mnt/* .
 
-echo Starting \[init "$@"\] >&2
-exec sbin/init "$@"
+echo Starting \[init "\$@"\] >&2
+exec sbin/init "\$@"
 EOF
 
 chmod 755 media/init
